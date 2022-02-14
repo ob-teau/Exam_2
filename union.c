62d7f78a049574563bc6cf8a1b125477a151e4f8
Alice <alice.coinus@gmx.fr>
2022-02-14 12:22:14 +0100

    union ok

--- union.c
+++ union.c
@@ -6,7 +6,7 @@
 /*   By: alice <acoinus@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/24 13:30:43 by alice             #+#    #+#             */
-/*   Updated: 2022/02/14 11:40:18 by alice            ###   ########.fr       */
+/*   Updated: 2022/02/14 12:21:55 by alice            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,22 +17,24 @@
 int	main(int ac, char **av)
 {
 	int	tab[260];
-	int	i;
+	int	i = 0;
 
-	i = 0;
-	while (i < 260)
-		tab[i++] = 0;
-	i = 0;
-	while (++i < 3)
+	if (ac == 3)
 	{
-		while(*av[i])
+		while (i < 260)
+			tab[i++] = 0;
+		i = 0;
+		while (++i < 3)
 		{
-			if (!tab[(int)*av[i]])
+			while(*av[i])
 			{
-				tab[(int)*av[i]] = 1;
-				write(1, av[i], 1);
+				if (!tab[(int)*av[i]])
+				{
+					tab[(int)*av[i]] = 1;
+					write(1, av[i], 1);
+				}
+				++av[i];
 			}
-			++av[i];
 		}
 	}
 	write(1, "\n", 1);
